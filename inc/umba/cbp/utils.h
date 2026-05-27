0f931f835afd0030a2d62e0d25678bca8d6038fd
al-martyn1 <amart@mail.ru>
2026-05-28 00:34:59 +0300

    Настроил

---
+++ inc/umba/cbp/utils.h
@@ -0,0 +1,38 @@
+/*! \file
+    \brief Различные утилиты для umba::cbp
+ */
+
+#pragma once
+
+//--------------------------------------------------------------------------------------------------------------------
+#include "umba/filename.h"
+#include "umba/filesys.h"
+
+//--------------------------------------------------------------------------------------------------------------------
+
+
+
+//--------------------------------------------------------------------------------------------------------------------
+// #include "umba/cbp/utils.h"
+// umba::chrome_devtools_protocol::utils::
+// umba::cdp::utils::
+
+namespace umba {
+namespace chrome_devtools_protocol {
+namespace utils {
+
+
+
+} // namespace utils
+} // namespace chrome_devtools_protocol
+
+namespace cdp = chrome_devtools_protocol;
+
+} // namespace umba
+
+// #include "umba/cbp/utils.h"
+// umba::chrome_devtools_protocol::utils::
+// umba::cdp::utils::
+
+//--------------------------------------------------------------------------------------------------------------------
+
